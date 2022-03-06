f9b83d48c80d00c30398586981e7b5b628e6ce68
Alkhatib99 <aboodjob@gmail.com>
2022-03-06 10:53:21 +0300

    edit the function

--- array-reverse/array_reverse/array_reverse.py
+++ array-reverse/array_reverse/array_reverse.py
@@ -1,11 +1,8 @@
-def reverse_array(arr):
-    i=0
-    j=len(arr)-1
-    while i<j:
-        arr[i],arr[j]=arr[j],arr[i]
-        i+=1
-        j-=1
-    return arr
+def reverseArray(list):
+    answer = []
+    for i in range(len(list)-1, -1, -1):
+        answer.append(list[i])
+    return answer
 
 
 
