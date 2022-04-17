f117cbe738d1f7a6eb6e281886bbda76c634b088
Alkhatib99 <aboodjob@gmail.com>
2022-04-17 08:39:43 +0300

    adding stack and queue

---
+++ stack_and_queue/stack_and_queue/node.py
@@ -0,0 +1,10 @@
+class Node:
+    """
+    The Node class has two properities for the va;ue stored in the node and pointer to next node
+    """
+    def __init__(self,value):
+        """
+        The initialization of the Node class
+        """
+        self.value =value
+        self.next = None
---
+++ stack_and_queue/stack_queue_pseudo/README.md
@@ -0,0 +1,5 @@
+# Challenge 11 - Stack queue pseudo
+
+## Challenge
+
+Implement a Queue using two Stacks.
